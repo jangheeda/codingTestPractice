a53ef5bf0f2e945f4f0bfbb0e9c89c1baeb95009
장희 <136553440+jangheeda@users.noreply.github.com>
2023-07-11 09:10:02 +0900

    Update 문자열 다루기 기본.java

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,14 +1,12 @@
 class Solution {
     public boolean solution(String s) {
         boolean answer = true;
-        char[] arr = s.toCharArray();
-        
+        char[] arr = s.toCharArray();        
         
         if (s.length() != 4 && s.length() != 6) {
             return false;
         }
         for (int i = 0; i < s.length(); ++i) {
-            System.out.print(arr[i] + " ");
             if (arr[i] < '0' || arr[i] > '9') return false;
         }
         return true;
