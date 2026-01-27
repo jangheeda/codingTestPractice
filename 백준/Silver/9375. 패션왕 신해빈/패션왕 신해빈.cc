e7da724a39c4af1438e919320d6831c803b5ee91
장희 <136553440+jangheeda@users.noreply.github.com>
2026-01-28 01:08:54 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+int t, n;
+string a, b;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> t;
+    while(t--) {
+        map<string, int> mp;
+        cin >> n;
+        for(int i = 0; i< n; i++) {
+            cin >> a >> b;
+            mp[b]++;
+        }
+        ll ret = 1;
+        for(auto c : mp) {
+            ret *= ((ll)c.second + 1);
+        }
+        ret--;
+        cout << ret << "\n";
+    }
+    return 0;
+}
