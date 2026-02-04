7d61d6851c6d94049b49b26dc6674cb9ea8d2b6f
장희 <136553440+jangheeda@users.noreply.github.com>
2026-02-05 00:00:37 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+ll a, b, c;
+
+ll go(ll a, ll b) {
+    if(b == 1) return a % c;
+
+    ll ret = go(a, b / 2);
+    ret = (ret * ret) % c;
+    if(b % 2) ret = (ret * a) % c; // 홀수개 일 경우 한번 곱해준다
+    return ret;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> a >> b >> c;
+    cout << go(a, b) << "\n";
+    return 0;
+}
