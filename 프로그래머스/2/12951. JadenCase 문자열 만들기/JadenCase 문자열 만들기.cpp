a6758650a58c02cb7521a69823d3f85baf66065a
장희 <136553440+jangheeda@users.noreply.github.com>
2026-04-28 18:13:34 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 3.69 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    bool isFirst = true;
+    
+    for(int i = 0; i < s.length(); i++) {
+        if(s[i] == ' ') {
+            isFirst = true;
+        } else if (isFirst) {
+            s[i] = toupper(s[i]);
+            isFirst = false;
+        } else {
+            s[i] = tolower(s[i]);
+        }
+    }
+    
+    
+    return s;
+}
